89a9a118c48ec329a1528dac4f3ac5d90732f914
Lovelessing <2773128204@qq.com>
2021-07-22 16:38:09 +0800

    Add files via upload

---
+++ fireware/STM32/FOC_POS_CTR/MDK-ARM/RTE/_foc_pmsm_m1_encoder_pos/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'foc_pmsm_m1_encoder_pos' 
+ * Target:  'foc_pmsm_m1_encoder_pos' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
